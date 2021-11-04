4fd88340dd20308758f7b897a88d668d0f68abc7
Nguyen Hoang Linh <linh072217@gmail.com>
2021-11-04 14:49:00 +0700

    update variable name data

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
 from sklearn.linear_model import LinearRegression
-housing_price_data = pd.read_csv("data/HousingPrices-Amsterdam-August-2021.csv")
-# housing_price_data.info()
+house_price_data = pd.read_csv("data/HousingPrices-Amsterdam-August-2021.csv")
+# 
+house_price_data.dropna(inplace=True)
 
-# clean data
-print(housing_price_data.head().to_string())
-housing_price_data.dropna(inplace=True)
-# housing_price_data["Address"] = housing_price_data["Address"].str.extract("(\w|\s)+(?=,)")
-housing_price_data.drop(["Unnamed: 0"], axis=1, inplace=True)
-housing_price_data["Address"] = pd.factorize(housing_price_data.Address)[0] + 1
-housing_price_data["Zip"] = housing_price_data["Zip"].str.extract("(\d+)")
-print(housing_price_data.Zip.min())
-print(housing_price_data.Zip.max())
-y = housing_price_data["Price"]
-housing_price_data.drop(["Price"], axis=1, inplace=True)
-# housing_price_data.drop(["Zip"], axis=1, inplace=True)
-X_train, X_test, y_train, y_test = train_test_split(housing_price_data, y, test_size=3/10.0, random_state=0)
+# house_price_data["Address"] = house_price_data["Address"].str.extract("(\w|\s)+(?=,)")
+house_price_data.drop(["Unnamed: 0"], axis=1, inplace=True)
+house_price_data["Address"] = pd.factorize(house_price_data.Address)[0] + 1
+house_price_data["Zip"] = house_price_data["Zip"].str.extract("(\d+)")
+print(house_price_data.Zip.min())
+print(house_price_data.Zip.max())
+y = house_price_data["Price"]
+house_price_data.drop(["Price"], axis=1, inplace=True)
+# house_price_data.drop(["Zip"], axis=1, inplace=True)
+X_train, X_test, y_train, y_test = train_test_split(house_price_data, y, test_size=3/10.0, random_state=0)
 
 model = LinearRegression()
 model.fit(X_train, y_train)
@@ -25,3 +24,4 @@ print("real value", y_test.head(1))
 # print(model.coef_)
 # print(model.intercept_)
 print("Score :", round(model.score(X_test, y_test)*100, ndigits=2), "%")
+print("r2 score: ", r2_score(y_test,  model.predict(X_test)))
