47164479131553301aa6058fa6b2726b8c19d915
Nguyen Hoang Linh <linh072217@gmail.com>
2021-11-12 20:23:55 +0700

    evaluate Linear Regression From Library

--- EvaluateOtherModel.py
+++ EvaluateOtherModel.py
@@ -8,6 +8,7 @@ from sklearn.linear_model import Ridge
 from sklearn.linear_model import Lasso
 from sklearn.linear_model import ElasticNet
 from sklearn.linear_model import BayesianRidge
+from sklearn.linear_model import LinearRegression as LinearRegressionLibrary
 housing_price_data = pd.read_csv("data/HousingPrices-Amsterdam-August-2021.csv")
 
 # clean data
@@ -22,7 +23,7 @@ housing_price_data.drop(["Price"], axis=1, inplace=True)
 
 
 mse_file = open("evaluate/evaluate_mse.csv", mode="w", newline="")
-fieldnames = ['Loop', 'LinearRegression', 'Ridge', 'Lasso', 'ElasticNet', 'BayesianRidge']
+fieldnames = ['Loop', 'LinearRegression','LinearRegressionLibrary', 'Ridge', 'Lasso', 'ElasticNet', 'BayesianRidge']
 mse_csv = csv.DictWriter(mse_file, fieldnames=fieldnames)
 mse_csv.writeheader()
 score_file = open("evaluate/evaluate_r_squared.csv", mode="w", newline="")
@@ -30,24 +31,29 @@ score_csv = csv.DictWriter(score_file, fieldnames=fieldnames)
 score_csv.writeheader()
 for i in range(10):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=3 / 10.0, random_state=i)
+
     linear_model = LinearRegression()
+    linear_model_lib = LinearRegressionLibrary()
     ridge_model = Ridge()
     lasso_model = Lasso()
     elastic_model = ElasticNet()
     br_model = BayesianRidge()
 
+    linear_model_lib.fit(X_train, y_train)
     linear_model.fit_stochastic(X_train, y_train)
     ridge_model.fit(X_train, y_train)
     lasso_model.fit(X_train, y_train)
     elastic_model.fit(X_train, y_train)
     br_model.fit(X_train, y_train)
 
+    linear_lib_mse = mean_squared_error(y_true=y_test, y_pred=linear_model_lib.predict(X_test))
     linear_mse = mean_squared_error(y_true=y_test, y_pred=linear_model.predict(X_test))
     ridge_mse = mean_squared_error(y_true=y_test, y_pred=ridge_model.predict(X_test))
     lasso_mse = mean_squared_error(y_true=y_test, y_pred=lasso_model.predict(X_test))
     elastic_mse = mean_squared_error(y_true=y_test, y_pred=elastic_model.predict(X_test))
     br_mse = mean_squared_error(y_true=y_test, y_pred=br_model.predict(X_test))
 
+    linear_lib_score = linear_model_lib.score(X_test,y_test)
     linear_score = linear_model.r2_score(y_test, linear_model.predict(X_test))
     ridge_score = ridge_model.score(X_test, y_test)
     lasso_score = lasso_model.score(X_test, y_test)
@@ -56,6 +62,7 @@ for i in range(10):
     mse_csv.writerow({
         "Loop": i,
         "LinearRegression": linear_mse,
+        "LinearRegressionLibrary": linear_lib_mse,
         "Ridge": ridge_mse,
         "Lasso": lasso_mse,
         "ElasticNet": elastic_mse,
@@ -64,6 +71,7 @@ for i in range(10):
     score_csv.writerow({
         "Loop": i,
         "LinearRegression": round(linear_score*100, 3),
+        "LinearRegressionLibrary": round(linear_lib_score*100, 3),
         "Ridge": round(ridge_score*100, 3),
         "Lasso": round(lasso_score*100, 3),
         "ElasticNet": round(elastic_score*100, 3),
