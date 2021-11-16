f2c5532a1cc9cc3e5302195c8f9fdd2bef003848
Nguyen Hoang Linh <linh072217@gmail.com>
2021-11-16 15:25:32 +0700

    update LinearRegression
    update attribute, scaling, variable name, parameter, drop column

--- LinearRegression.py
+++ LinearRegression.py
@@ -5,41 +5,45 @@ from sklearn.metrics import r2_score
 
 
 class LinearRegression:
-    def __init__(self):
-        self.coef_ = np.array([])
-        self.intercept_ = 0.001
-        self.eta = 0.001
-        self.x = np.array([])
-        self.y = np.array([])
 
+    def __init__(self, eta=0.001, intercept_=0):
+        self.coef_ = np.array([])
+        self.intercept_ = intercept_
+        self.eta = eta
+        self.x_train = np.array([])
+        self.y_train = np.array([])
+    """
+       x = (x - mean(x))/(max(x) -  min(x))
+    """
     def feature_scaling(self, features):
         x = features.copy()
         n = len(x[0, :])
         for i in range(n):
-            if x[0, i] > 1:
+            if np.average(x[:, i]) > 1:
                 max_min = float(x[:, i].max() - x[:, i].min())
                 x[:, i] = (x[:, i] - x[:, i].mean()) / max_min
         return x.copy()
-
-
+    """
+      y = (y - mean(y))/(max(y) -  min(y))
+    """
     def y_scaling(self, y):
         n = len(y)
         for i in range(n):
-            if y[0] > 100:
+            if (np.average(y)) > 10:
                 max_min = float(y.max() - y.min())
                 y = (y - y.mean()) / max_min
         return y.copy()
 
-    def fit_stochastic(self, x, y):
-        self.x = x.copy()
-        self.y = y.copy()
-        m = len(x)
-        n = len(x[0, :])
-        self.coef_ = np.zeros((1, 5))
+    def fit_stochastic(self, x_train, y_train):
+        self.x_train = x_train.copy()
+        self.y_train = y_train.copy()
+        m = len(x_train)
+        n = len(x_train[0, :])
+        self.coef_ = np.zeros((n), dtype=np.float64)
         print("theta0=", self.intercept_)
         print("thetaj=", self.coef_)
-        x_scaling = self.feature_scaling(x)
-        y_scaling = self.y_scaling(y)
+        x_scaling = self.feature_scaling(x_train)
+        y_scaling = self.y_scaling(y_train)
         while True:
             for i in range(m):
                 h_i = np.sum(self.coef_*x_scaling[i, ]) + self.intercept_
@@ -54,48 +58,38 @@ class LinearRegression:
                     print("thetaj=", self.coef_)
                     return
 
-    # def fit_batch(self, x, y):
-    #     m = len(x)
-    #     n = len(x[0, :])
-    #     total0 = 0
-    #     total1 = 0
-    #     self.coef_ = np.random.randn(n)
-    #     x = self.feature_scaling(x)
-    #     while True:
-    #         for i in range(m):
-    #             h_i = self.intercept_ + np.sum(self.coef_ * x[i, ])
-    #             total0 += y[i] - h_i * 1
-    #             total1 += (y[i] - h_i) * x[i, ]
-    #         previous_theta0 = self.intercept_
-    #         previous_theta = self.coef_.copy()
-    #         self.intercept_ += self.eta * total0
-    #         self.coef_ += self.eta * total1
-    #         if abs(previous_theta0 - self.intercept_) < pow(10, -3) and np.abs(previous_theta - self.coef_) < np.full((1,5), pow(10,-3)):
-    #             break
-
     def fit_stochastic1(self, x, y):
+        self.eta = 0.01
+        self.intercept_ = 0
         m = len(x)
         n = len(x[0, :])
-        self.coef_ = np.array([-1.752, -0.25, -2.04, -0.703, 0.441])
+        self.coef_ = np.full((n), 0.5)
         print("theta0", self.intercept_)
         print("theta", self.coef_)
-        for i in range(2):
-            h_i = round(np.sum(self.coef_ * x[i,]) + self.intercept_, 3)
-            # previous = self.intercept_
+        for i in range(m):
+            print(x[i, ], y[i])
+            h_i = round(np.sum(self.coef_ * x[i, ]) + self.intercept_, 3)
             pre_theta0 = self.intercept_
             pre_theta = np.round(self.coef_.copy(),3)
             self.intercept_ -= np.round(self.eta * (h_i - y[i]) * 1, 3)
             self.coef_ -= np.round(self.eta * (h_i - y[i]) * x[i,], 3)
-            print("h(x) = {theta0} + {x1}*{theta1} + {x2}*{theta2} + {x3}*{theta3} + {x4}*{theta4} + {x5}*{theta5} = {hi}".format(
+            print(
+                "h(x) = {theta0} + x1*{theta1} + x2*{theta2} + x3*{theta3}".format(
+                    theta0=pre_theta0,
+                    theta1=pre_theta[0], theta2=pre_theta[1],
+                    theta3=pre_theta[2],
+                ))
+            print("h(x) = {theta0} + {x1}*{theta1} + {x2}*{theta2} + {x3}*{theta3}  = {hi}".format(
                 theta0=pre_theta0,
                 x1=x[i, 0],theta1=pre_theta[0], x2=x[i, 1],theta2=pre_theta[1],
-                x3=x[i, 2], theta3=pre_theta[2], x4=x[i, 3],theta4=pre_theta[3],
-                x5=x[i, 4], theta5=pre_theta[4], hi=h_i
+                x3=x[i, 2], theta3=pre_theta[2],hi=h_i
             ))
+            print("theta0 = theta0 - eta*(hi - y0)*x0")
             print("theta0 = {theta0} - {eta}*({hi} - {yi})*{xi} = {new_theta}".format(theta0=pre_theta0,
                                                                                       eta=self.eta, hi=h_i,
                                                                                       yi=y[i], xi=1, new_theta=self.intercept_))
             for j in range(n):
+                print("theta{i} = theta{i} - eta*(hi - y{i})*x{i}".format(i=i))
                 print("theta{i} = {thetai} - {eta}*({hi} - {yi})*{xi} = {new_thetai}".format(i=j+1, eta=self.eta,
                                                                                          thetai=pre_theta[j],
                                                                                          yi=y[i], xi=x[i, j],
@@ -107,15 +101,14 @@ class LinearRegression:
             print("=" * 50)
 
     def predict(self, features):
-        # print(features)
         features_scale = self.feature_scaling(features)
-        max_min = self.y.max() - self.y.min()
-        mean = self.y.mean()
-        y_pred = np.zeros(shape=(len(features_scale), 1))
+        max_min = self.y_train.max() - self.y_train.min()
+        mean = self.y_train.mean()
+        y_pred = np.zeros(shape=(len(features_scale)))
         for index, x in enumerate(features_scale):
             y_pred[index] = (np.sum(x*self.coef_) + self.intercept_)
             y_pred[index] = (y_pred[index] * max_min) + mean
-        return y_pred.reshape(-1)
+        return y_pred
 
     def mse_score(self, y_true, y_pred):
         y_true = np.array(y_true)
@@ -134,19 +127,19 @@ class LinearRegression:
 
 if __name__ == '__main__':
     house_price_data = pd.read_csv("data/HousingPrices-Amsterdam-August-2021.csv", index_col=0)
+    # preprocessing data
     house_price_data.dropna(inplace=True)
-    house_price_data["Address"] = pd.factorize(house_price_data.Address)[0] + 1
+    house_price_data.drop(axis=1, columns="Address", inplace=True)
     house_price_data["Zip"] = house_price_data["Zip"].str.extract("(\d+)").astype(float)
-    house_price_data.astype(float)
+    print(house_price_data.head().to_string())
+    # get X, Y
     Y = np.array(house_price_data["Price"])
     X = np.array(house_price_data[["Zip", "Area", "Room", "Lon", "Lat"]])
-    # print(house_price_data.head().to_string())
-    for i in range(3):
+    for i in range(5):
         X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=3 / 10.0, random_state=i)
-        model = LinearRegression()
+        model = LinearRegression(eta=0.001)
         model.fit_stochastic(np.float64(X_train), np.float64(y_train))
         y_pred = model.predict(X_test)
-        print(X_test,y_test)
         print(r2_score(y_test, y_pred))
         print("accuracy={}%".format(np.round(model.r2_score(y_test, y_pred)*100, 3)))
         print("="*50)
